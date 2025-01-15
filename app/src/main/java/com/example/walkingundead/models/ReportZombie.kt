34d1adf5fbbae08e1e3b82361ffd7e0a6b4de142
Sofia Lopes <sofiamartinslopes1@gmail.com>
2025-01-15 15:04:57 +0000

    Mudei class dos zombies porque crashava

--- app/src/main/java/com/example/walkingundead/models/ReportZombie.kt
+++ app/src/main/java/com/example/walkingundead/models/ReportZombie.kt
@@ -1,7 +1,10 @@
 package com.example.walkingundead.models
 
 data class ReportZombie(
-    var id: String = "", // Unique ID
-    val location: String, // Location as "latitude,longitude"
-    var emailRegisteredBy: String = ""
-)
+    var id: String? = null, // Unique ID
+    val location: String? = null, // Location as "latitude,longitude"
+    val emailRegisteredBy: String? = null
+) {
+    // No-argument constructor required by Firebase
+    constructor() : this(null, null, null)
+}
